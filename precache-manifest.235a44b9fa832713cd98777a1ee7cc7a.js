dfd5268310b9a55fb91349e869eb02fbc21cd534
xswapexchange <89673587+xswapexchange@users.noreply.github.com>
2021-12-20 16:49:19 +0700

    Add files via upload

---
+++ precache-manifest.235a44b9fa832713cd98777a1ee7cc7a.js
@@ -0,0 +1,358 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c626e78b506a8457d972d5d42d70e241",
+    "url": "./index.html"
+  },
+  {
+    "revision": "969c5edf24a61e404e3c",
+    "url": "./static/css/4.f04942fe.chunk.css"
+  },
+  {
+    "revision": "4c7b55f6809ff2087dbf",
+    "url": "./static/css/main.873a611f.chunk.css"
+  },
+  {
+    "revision": "f4bf8c654ba4a0a05f95",
+    "url": "./static/js/0.73f8a67a.chunk.js"
+  },
+  {
+    "revision": "051be869993b5809aa65",
+    "url": "./static/js/1.874382bd.chunk.js"
+  },
+  {
+    "revision": "969c5edf24a61e404e3c",
+    "url": "./static/js/4.04fe1d82.chunk.js"
+  },
+  {
+    "revision": "5f9b6e7e0c34704d492dae42cf9cac81",
+    "url": "./static/js/4.04fe1d82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cfce48ddea03fa153fc",
+    "url": "./static/js/5.ec810724.chunk.js"
+  },
+  {
+    "revision": "caaa9c24a9c77eaead93960f555b5a26",
+    "url": "./static/js/5.ec810724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36ec180cc1c3d6c9f1dc",
+    "url": "./static/js/6.63fabd8a.chunk.js"
+  },
+  {
+    "revision": "2cba3b6ba96af304c685",
+    "url": "./static/js/7.8a09ac91.chunk.js"
+  },
+  {
+    "revision": "58564bdec932db02e13e",
+    "url": "./static/js/8.db5adcdf.chunk.js"
+  },
+  {
+    "revision": "f25d4783243528872ca3",
+    "url": "./static/js/9.d0c66084.chunk.js"
+  },
+  {
+    "revision": "ef998acd8f65ae6b9b420793d7484b0e",
+    "url": "./static/js/9.d0c66084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c7b55f6809ff2087dbf",
+    "url": "./static/js/main.7ddc3f25.chunk.js"
+  },
+  {
+    "revision": "6ec4c050f28aecb452ff",
+    "url": "./static/js/runtime-main.a506b1f3.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "./static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "./static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "07e69b53f8cf91da94096d1abf484302",
+    "url": "./static/media/Inter-BlackItalic.07e69b53.woff"
+  },
+  {
+    "revision": "daa1ca3cebd42cf5ebd53d9a6942b2cf",
+    "url": "./static/media/Inter-BlackItalic.daa1ca3c.woff2"
+  },
+  {
+    "revision": "79260e5b693fac2ee373b659f01a2bdf",
+    "url": "./static/media/Inter-Bold.79260e5b.woff"
+  },
+  {
+    "revision": "aed27700d84e327fda56b4a427b03061",
+    "url": "./static/media/Inter-Bold.aed27700.woff2"
+  },
+  {
+    "revision": "8ef77a031c5b96e53f2eeeb009232181",
+    "url": "./static/media/Inter-BoldItalic.8ef77a03.woff2"
+  },
+  {
+    "revision": "e0879d64763e509f309940120e7d1f5a",
+    "url": "./static/media/Inter-BoldItalic.e0879d64.woff"
+  },
+  {
+    "revision": "38bc51bccb7dd07b2752c11888cbdc89",
+    "url": "./static/media/Inter-ExtraBold.38bc51bc.woff"
+  },
+  {
+    "revision": "92d16aee8fb5f5c5cfd660b2d07e1148",
+    "url": "./static/media/Inter-ExtraBold.92d16aee.woff2"
+  },
+  {
+    "revision": "0e4b21ebad8d568078450713ee9339cf",
+    "url": "./static/media/Inter-ExtraBoldItalic.0e4b21eb.woff"
+  },
+  {
+    "revision": "57ea76d011f6fff8cb7013b783bad58e",
+    "url": "./static/media/Inter-ExtraBoldItalic.57ea76d0.woff2"
+  },
+  {
+    "revision": "4bd040df09ea4765148b5743b8493e5f",
+    "url": "./static/media/Inter-ExtraLight.4bd040df.woff"
+  },
+  {
+    "revision": "4d9f96f8504cd034ef7e5f25434ca550",
+    "url": "./static/media/Inter-ExtraLight.4d9f96f8.woff2"
+  },
+  {
+    "revision": "54d3d9a59ea904ebd11c3ac308893bfb",
+    "url": "./static/media/Inter-ExtraLightItalic.54d3d9a5.woff2"
+  },
+  {
+    "revision": "84c26656e02913a54131fc64431212ae",
+    "url": "./static/media/Inter-ExtraLightItalic.84c26656.woff"
+  },
+  {
+    "revision": "9528384cf7aebfa81a198626cb05f5e8",
+    "url": "./static/media/Inter-Italic.9528384c.woff2"
+  },
+  {
+    "revision": "e4ad3666e223c64e4df963cec740bf09",
+    "url": "./static/media/Inter-Italic.e4ad3666.woff"
+  },
+  {
+    "revision": "5baca21acf845c8e746f08675f40300b",
+    "url": "./static/media/Inter-Light.5baca21a.woff2"
+  },
+  {
+    "revision": "b9920de0749addeb5a53462fb5bbe070",
+    "url": "./static/media/Inter-Light.b9920de0.woff"
+  },
+  {
+    "revision": "0555a46cda7fc3406a875d6bacf8fcff",
+    "url": "./static/media/Inter-LightItalic.0555a46c.woff"
+  },
+  {
+    "revision": "adc70179a9e60e14f2a4f7d106166004",
+    "url": "./static/media/Inter-LightItalic.adc70179.woff2"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "./static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "./static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "417907d2be56d39b2a9c0b52fc8a00e8",
+    "url": "./static/media/Inter-MediumItalic.417907d2.woff"
+  },
+  {
+    "revision": "565a7104c497ccb1bf12832d8b341861",
+    "url": "./static/media/Inter-MediumItalic.565a7104.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "./static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "./static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "1db6c55ca0f2a97fbce3b0b3903a7bb7",
+    "url": "./static/media/Inter-SemiBold.1db6c55c.woff"
+  },
+  {
+    "revision": "dd8a55ef7058cdaeb96ef9fc65344726",
+    "url": "./static/media/Inter-SemiBold.dd8a55ef.woff2"
+  },
+  {
+    "revision": "81678d1a50a895cc1232f3e287976083",
+    "url": "./static/media/Inter-SemiBoldItalic.81678d1a.woff"
+  },
+  {
+    "revision": "ac201e30486307c75c5038f61df9fd9f",
+    "url": "./static/media/Inter-SemiBoldItalic.ac201e30.woff2"
+  },
+  {
+    "revision": "850febbe5e8a7625c5603061d2e7c0ce",
+    "url": "./static/media/Inter-Thin.850febbe.woff2"
+  },
+  {
+    "revision": "ead42837911f0b55b0877c12b50d9157",
+    "url": "./static/media/Inter-Thin.ead42837.woff"
+  },
+  {
+    "revision": "a76db06556290ed3bd198a72b24d8006",
+    "url": "./static/media/Inter-ThinItalic.a76db065.woff"
+  },
+  {
+    "revision": "e08d9b2a5079b81cc39804c2a7406254",
+    "url": "./static/media/Inter-ThinItalic.e08d9b2a.woff2"
+  },
+  {
+    "revision": "2690e3c25733fb852b55b3f8d651a431",
+    "url": "./static/media/Inter-italic.var.2690e3c2.woff2"
+  },
+  {
+    "revision": "90e8f61d26f65b5ff0acc45ddf6740ea",
+    "url": "./static/media/Inter-roman.var.90e8f61d.woff2"
+  },
+  {
+    "revision": "4b976905f2be1cb9201f5d94afd9edd2",
+    "url": "./static/media/Inter.var.4b976905.woff2"
+  },
+  {
+    "revision": "720dcf01a18186b4a77cf47e51dfce7a",
+    "url": "./static/media/KaushanScript-Regular.720dcf01.woff2"
+  },
+  {
+    "revision": "18ee0d0af8b384cac85c606c16d25c1a",
+    "url": "./static/media/NotoSans-Regular.18ee0d0a.woff2"
+  },
+  {
+    "revision": "114fab910be089671a355764e2326334",
+    "url": "./static/media/arrow-down-blue.114fab91.svg"
+  },
+  {
+    "revision": "4d4a6a99f8bc24af0fcf805146d38a46",
+    "url": "./static/media/arrow-down-grey.4d4a6a99.svg"
+  },
+  {
+    "revision": "337ad716bd89163e2a9c3495b7e0f029",
+    "url": "./static/media/arrow-right-white.337ad716.png"
+  },
+  {
+    "revision": "e96d8158ff6d3087ab15e43e64fbb47e",
+    "url": "./static/media/arrow-right.e96d8158.svg"
+  },
+  {
+    "revision": "75bf5d4b0204729da6fd5aa3f3a615f7",
+    "url": "./static/media/background-dark.75bf5d4b.jpg"
+  },
+  {
+    "revision": "02830c3de113a27ea52bbfa7abfa5293",
+    "url": "./static/media/background-light.02830c3d.jpg"
+  },
+  {
+    "revision": "83810dc519497ff2e1ffd920ea22d3e2",
+    "url": "./static/media/binanceChainWallet.83810dc5.svg"
+  },
+  {
+    "revision": "e62a99c33b7eafddf701893750cedeb9",
+    "url": "./static/media/blue-loader.e62a99c3.svg"
+  },
+  {
+    "revision": "5dd950ec66be581e8da4376f49a19d77",
+    "url": "./static/media/circle-grey.5dd950ec.svg"
+  },
+  {
+    "revision": "716403ba670c3577abbea8e66f428721",
+    "url": "./static/media/circle.716403ba.svg"
+  },
+  {
+    "revision": "0d5cc84b7132bf341c2d53b3a8a049e8",
+    "url": "./static/media/coinbaseWalletIcon.0d5cc84b.svg"
+  },
+  {
+    "revision": "80a3ae258c229f3c874a8acdf65adeea",
+    "url": "./static/media/dropdown-blue.80a3ae25.svg"
+  },
+  {
+    "revision": "50dbd07d8e7428d04464644f7cf819cf",
+    "url": "./static/media/dropdown.50dbd07d.svg"
+  },
+  {
+    "revision": "345dad7494f835eb9ae6c21c2d3a25f4",
+    "url": "./static/media/dropup-blue.345dad74.svg"
+  },
+  {
+    "revision": "089df9509ced49ba3c1d1cabb9ed0245",
+    "url": "./static/media/immunefi.089df950.png"
+  },
+  {
+    "revision": "50c67f3cdd04281013ef95e92fc7244e",
+    "url": "./static/media/link.50c67f3c.svg"
+  },
+  {
+    "revision": "52eac682e8d399b4a8d1d48e38a60535",
+    "url": "./static/media/magnifying-glass.52eac682.svg"
+  },
+  {
+    "revision": "7394328d226d76255ba98404bbb147ff",
+    "url": "./static/media/mathWallet.7394328d.png"
+  },
+  {
+    "revision": "981ecca4e2e244ff635162cbb11b69ac",
+    "url": "./static/media/menu.981ecca4.svg"
+  },
+  {
+    "revision": "023762b6aec2a2249b8fdfb638f00ef3",
+    "url": "./static/media/metamask.023762b6.png"
+  },
+  {
+    "revision": "899a4aa9bf85aba699112cc7cab6578e",
+    "url": "./static/media/plus-blue.899a4aa9.svg"
+  },
+  {
+    "revision": "62281ca8cea976c7f20862db5d12728e",
+    "url": "./static/media/plus-grey.62281ca8.svg"
+  },
+  {
+    "revision": "51affe3e492ed7f0fe6566c086ca615e",
+    "url": "./static/media/plus-white.51affe3e.svg"
+  },
+  {
+    "revision": "b234b2bfa0417c7e8711c3a8d17afeec",
+    "url": "./static/media/portisIcon.b234b2bf.png"
+  },
+  {
+    "revision": "bec008d0c3c76677be086302b5aa3fa7",
+    "url": "./static/media/pxl-logo.bec008d0.png"
+  },
+  {
+    "revision": "1761ff9cb139a2ba2a3a485172eb094c",
+    "url": "./static/media/question-mark.1761ff9c.svg"
+  },
+  {
+    "revision": "63be1e247274b62bc269445bf114d497",
+    "url": "./static/media/question.63be1e24.svg"
+  },
+  {
+    "revision": "a98424922149274b8a364358d02da9fe",
+    "url": "./static/media/spinner.a9842492.svg"
+  },
+  {
+    "revision": "4ac5068ef3826b8bf110d47adc5456ea",
+    "url": "./static/media/trustWallet.4ac5068e.svg"
+  },
+  {
+    "revision": "edcc1ab5dde5cb3d5cf134c4aade641b",
+    "url": "./static/media/trustWallet.edcc1ab5.png"
+  },
+  {
+    "revision": "5e81cac236fd057cb686399a8fa2ea57",
+    "url": "./static/media/walletConnectIcon.5e81cac2.svg"
+  },
+  {
+    "revision": "5b8e218668bfea1d44b887bd042f6a52",
+    "url": "./static/media/x.5b8e2186.svg"
+  }
+]);
